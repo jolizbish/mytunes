0c21e49e053aa8544af2100cedfcc550fc729958
Hack Reactor Student <student@hackreactor.com>
2017-05-22 20:02:34 -0500

    got through a vast majority of the tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,36 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+    
+    this.on('ended', function() {
+      console.log('song ended');
+      this.shift();
+      if (this.length > 0) {
+        this.playFirst();
+      }
+    }),
+    
+    this.on('dequeue', function() {
+      var index = _.indexOf(this);
+      this.remove(this.at(index));
+    });
+    
+    this.on('enqueue', function(song) {
+      this.push(song);
+    });
+  },
+  
+  playFirst: function() {
+    this.at(0).play();
+  },
+  
+  dequeue: function() {
+    this.shift();
+    this.playFirst();
   }
-
 });
--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -6,6 +6,14 @@ var PlayerView = Backbone.View.extend({
   el: '<audio controls autoplay />',
 
   initialize: function() {
+    // this.on('ended', function() {
+    //   this.collection.shift();
+    //   this.playFirst();
+    // });
+    // this.model.dequeue();
+    $('audio').on('ended', function() {
+      $el.dequeue();
+    });
   },
 
   setSong: function(song) {
